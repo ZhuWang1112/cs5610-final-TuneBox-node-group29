624a575e02d64a581ac26df3c8eb400a5c1d3ffc
ZhuWang <wang.zhu1@northeastern.edu>
2023-04-05 21:15:22 -0700

    fix

--- dao/schemata/user-schema.js
+++ dao/schemata/user-schema.js
@@ -7,6 +7,15 @@ const userSchema = new mongoose.Schema({
     gender: { type: String, required: true },
     isAdmin: { type: Boolean, default: false },
     isVip: { type: Boolean, default: false },
+    playlistsCount: { type: Number, default: 0 },
 }, { collection: "users" });
+
+userSchema.method.generateAuthToken = function () {
+    // return token
+    return jwt.sign(
+        {_id: this._id, name: this.name, isAdmin: this.isAdmin},
+        process.env.JWTPRIVATEKEY,
+        {expiresIn: "7d"});
+}
 
 export default userSchema;
