afcf7e3d6bd80d48d8f086242f73cefc205f19d9
cst950928 <cst950928@hotmail.com>
2023-04-08 18:40:35 -0700

    enable-dotenv

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from "dotenv";
 import mongoose from "mongoose";
 import PlaylistController from "./controllers/playlist-controller.js";
 import UserController from "./controllers/user-controller.js";
-import {DB_CONNECTION_STRING} from "./env.js";
 const app = express();
-const CONNECTION_STR = DB_CONNECTION_STRING
 
 app.use(cors());
 app.use(express.json());
-const CONNECTION_STRING = CONNECTION_STR || 'mongodb://localhost:27017';
+dotenv.config();
+mongoose.set("strictQuery", false);
+const CONNECTION_STRING = process.env.DB_CONNECTION || 'mongodb://localhost:27017';
 mongoose.connect(CONNECTION_STRING);
 
 app.get('/hello', (req, res) => {res.send('Hello World!')})
 
 PlaylistController(app);
 UserController(app);
-app.listen(process.env.PORT || 4000,() => {
-    // connect();
-    console.log("Connected to backend!");
-})
+app.listen(process.env.PORT || 4000)
