87b429d6f4726fec3a4464b56fad5be86a3dc8c4
cst950928 <cst950928@hotmail.com>
2023-04-12 20:10:29 -0700

    update playlist rating when create and delete comment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -3,15 +3,17 @@ import * as playlistDao from "../dao/playlist-dao.js";
 import * as userDao from "../dao/user-dao.js";
 
 // create a comment record
-const createComment = async(req, res) => {
-    const newComment = req.body;
-    const insertedComment = await commentDao.createComment(newComment);
-    res.json(insertedComment);
-}
+const createComment = async (req, res) => {
+  const newComment = req.body;
+  const { rating, playlist, newAvgRating } = newComment;
+  const insertedComment = await commentDao.createComment(newComment);
+  await playlistDao.updatePlaylist({ _id: playlist, rating: newAvgRating });
+  res.json(insertedComment);
+};
 
 // find the comments belongs to the user
 // also get the playlistname and artist name for displaying in Profile page
-const findComments = async(req, res) => {
+const findComments = async (req, res) => {
   const uid = req.params.uid;
   const comments = await commentDao.findCommentsByUserId(uid);
   const playlistIds = comments.map((c) => c.playlist);
@@ -41,14 +43,25 @@ const findComments = async(req, res) => {
     };
   });
   res.json(commentsWithDetails);
-}
+};
 
 // delete comment by commentId(_id in comment schema)
-const deleteComments = async(req, res) => {
-    const cid = req.params.cid;
-    const status = await commentDao.deleteComment(cid);
-    res.json(status);
-}
+const deleteComments = async (req, res) => {
+  const commentObj = req.body.commentObj;
+  const playlistObj = await playlistDao.findPlaylistById(commentObj.playlist);
+  const number = await commentDao.findCommentNumberByPlaylist(
+    commentObj.playlist
+  );
+  if (number === 1) {
+    playlistObj.rating = 0;
+  } else {
+    playlistObj.rating =
+      ((playlistObj.rating * number - commentObj.rating) * 1.0) / (number - 1);
+  }
+  await playlistDao.updatePlaylist(playlistObj);
+  const status = await commentDao.deleteComment(commentObj._id);
+  res.json(status);
+};
 
 const findCommentsByPlaylist = async (req, res) => {
   const pid = req.params.pid;
@@ -60,5 +73,5 @@ export default (app) => {
   app.post("/api/comment", createComment);
   app.get("/api/comment/:uid", findComments);
   app.get("/api/comment/playlist/:pid", findCommentsByPlaylist);
-  app.delete("/api/comment/:cid", deleteComments);
+  app.delete("/api/comment", deleteComments);
 };
