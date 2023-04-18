d0d81b91cff2802ac36d83c1a7f8c7684c1d8067
ZhuWang <wang.zhu1@northeastern.edu>
2023-04-18 00:02:47 -0700

    add remote API for detial page

--- controllers/remoteApi-controller.js
+++ controllers/remoteApi-controller.js
@@ -15,7 +15,66 @@ const getTrack = async (req,res) => {
     res.json(response.data);
 }
 
+const getAlbumsByArtistId = async (req,res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://spotify23.p.rapidapi.com/artist_albums/',
+        params: {id: req.params.apiArtistId, offset: '0', limit: '100'},
+        headers: {
+            'X-RapidAPI-Key': '230ac653ebmsh094464f7969dbdbp1121a1jsnc05a7bf66309',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    const response = await axios.request(options);
+    // console.log("hello +++" + JSON.stringify(response.data));
+    let n = response.data.data.artist.discography.albums.totalCount;
+    let albums = [];
+    for(let i = 0; i < n; i++){
+        albums[i] = {
+            apiAlbumId: response.data.data.artist.discography.albums.items[i].releases.items[0].id,
+            title: response.data.data.artist.discography.albums.items[i].releases.items[0].name,
+            img: response.data.data.artist.discography.albums.items[i].releases.items[0].coverArt.sources[0].url,
+            date: response.data.data.artist.discography.albums.items[i].releases.items[0].date.year,
+            tracksNum: response.data.data.artist.discography.albums.items[i].releases.items[0].tracks.totalCount,
+        }
+    }
+    res.json(albums);
+}
+
+const getTracksByAlbumId = async (req,res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://spotify23.p.rapidapi.com/albums/',
+        params: {ids: req.params.apiAlbumId},
+        headers: {
+            'X-RapidAPI-Key': '230ac653ebmsh094464f7969dbdbp1121a1jsnc05a7bf66309',
+            'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+        }
+    };
+    const response = await axios.request(options);
+    // console.log("hello +++" + JSON.stringify(response.data));
+
+    let n = response.data.albums[0].total_tracks;
+
+    let tracks = [];
+    for(let i = 0; i < n; i++){
+        tracks[i] = {
+            apiSongId: response.data.albums[0].tracks.items[i].id,
+            mp3Url: response.data.albums[0].tracks.items[i].preview_url,
+            artist: response.data.albums[0].tracks.items[i].artists[0].name,
+            apiArtistId: response.data.albums[0].tracks.items[i].artists[0].id,
+            songName: response.data.albums[0].tracks.items[i].name,
+            duration: response.data.albums[0].tracks.items[i].duration_ms,
+        }
+    }
+    res.json(tracks);
+}
+
 export default (app) => {
     // get Track
     app.get("/api/remoteApi/songs/:apiSongId", getTrack);
+    // get Albums
+    app.get("/api/remoteApi/albums/:apiArtistId", getAlbumsByArtistId);
+    // get Tracks
+    app.get("/api/remoteApi/tracks/:apiAlbumId", getTracksByAlbumId);
 }
